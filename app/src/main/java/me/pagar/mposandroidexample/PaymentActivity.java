74da9ced14fdb632a14cf4d5004afb70fec4a1f7
Lucas Duarte <lc.sales.duarte@gmail.com>
2017-10-14 17:46:28 -0300

    bluetooth: fix bluetooth receiver
    Calling bluetooth receiver only after have the permission.
    
    BroadcastReceivers must be unregistered, or it will break when you
    change the activity and comes back.

--- app/src/main/java/me/pagar/mposandroidexample/PaymentActivity.java
+++ app/src/main/java/me/pagar/mposandroidexample/PaymentActivity.java
@@ -1,6 +1,5 @@
 package me.pagar.mposandroidexample;
 
-import android.Manifest;
 import android.bluetooth.BluetoothAdapter;
 import android.bluetooth.BluetoothDevice;
 import android.content.BroadcastReceiver;
@@ -8,18 +7,26 @@ import android.content.Context;
 import android.content.Intent;
 import android.content.IntentFilter;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.v4.app.ActivityCompat;
+import android.support.v4.app.ActivityCompat.OnRequestPermissionsResultCallback;
 import android.support.v7.app.AppCompatActivity;
-import android.util.Log;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
+import android.widget.Toast;
 
 import java.util.ArrayList;
 
 import me.pagar.mposandroidexample.listeners.PayListener;
 
+import static android.Manifest.permission.ACCESS_COARSE_LOCATION;
+import static android.content.pm.PackageManager.PERMISSION_GRANTED;
 
-public class PaymentActivity extends AppCompatActivity {
+
+public class PaymentActivity extends AppCompatActivity implements OnRequestPermissionsResultCallback {
 
 	private ListView listView;
 	private ArrayList<String> mDeviceList = new ArrayList<>();
@@ -36,21 +43,64 @@ public class PaymentActivity extends AppCompatActivity {
 		listView.setOnItemClickListener(payListener);
 
 		getBluetoothScanPermission();
+	}
 
+	@Override
+	protected void onPause() {
+		super.onPause();
+
+		if (registered) {
+			unregisterReceiver(mReceiver);
+		}
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		MenuInflater inflater = getMenuInflater();
+		inflater.inflate(R.menu.payment, menu);
+		return true;
+	}
+
+
+	int REQUEST_CODE = 1;
+	String PERMISSION = ACCESS_COARSE_LOCATION;
+	int GRANTED = PERMISSION_GRANTED;
+	public void getBluetoothScanPermission() {
+		ActivityCompat.requestPermissions(this, new String[]{PERMISSION}, REQUEST_CODE);
+	}
+
+	@Override
+	public void onRequestPermissionsResult(
+		int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults
+	) {
+		if (requestCode == REQUEST_CODE)
+		{
+			for (int p = 0; p < permissions.length; p++) {
+				String permission = permissions[p];
+				if (permission.equals(PERMISSION)) {
+					if (grantResults[p] == GRANTED) {
+						scanBluetooth();
+					} else {
+						Toast.makeText(
+							this, R.string.denyPermission, Toast.LENGTH_LONG
+						).show();
+					}
+				}
+			}
+		}
+	}
+
+	public void scanBluetooth()
+	{
 		BluetoothAdapter mBluetoothAdapter = BluetoothAdapter.getDefaultAdapter();
 		mBluetoothAdapter.startDiscovery();
 
 		IntentFilter filter = new IntentFilter(BluetoothDevice.ACTION_FOUND);
 		registerReceiver(mReceiver, filter);
+		registered = true;
 	}
 
-	public void getBluetoothScanPermission() {
-		int MY_PERMISSIONS_REQUEST_ACCESS_COARSE_LOCATION = 1;
-		ActivityCompat.requestPermissions(this,
-				new String[]{Manifest.permission.ACCESS_COARSE_LOCATION},
-				MY_PERMISSIONS_REQUEST_ACCESS_COARSE_LOCATION);
-	}
-
+	private boolean registered = false;
 	private final BroadcastReceiver mReceiver = new BroadcastReceiver() {
 		public void onReceive(Context context, Intent intent) {
 			String action = intent.getAction();
